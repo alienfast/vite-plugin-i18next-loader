c86b45205b53f0e338b66d80baeea19fbc13f29b
Kevin Ross <kevin.ross@alienfast.com>
2022-10-13 08:44:03 -0500

    pathOverride.test is now converted [skip ci]

--- src/__tests__/basic.test.ts
+++ src/__tests__/basic.test.ts
@@ -1,27 +1,17 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 import * as path from 'node:path'
 
-import { merge } from 'lodash-es'
 import { beforeEach, describe, expect, it } from 'vitest'
 
-import factory, { Options, resolvedVirtualModuleId } from '../index'
-import { esm, LoaderPlugin, ThisScope } from './util'
+import factory, { resolvedVirtualModuleId } from '../index'
+import { esm, ThisScope } from './util'
 
 describe('basic', () => {
   for (const type of ['yaml', 'json']) {
+    // eslint-disable-next-line unicorn/prefer-module
+    const appLocalesDir = path.join(__dirname, `./data/basic-app-${type}/locales`)
     describe(type, () => {
       let thisScope: ThisScope
-      function newPlugin(options: Partial<Options> = {}) {
-        return factory(
-          merge(
-            {
-              // eslint-disable-next-line unicorn/prefer-module
-              paths: [path.join(__dirname, `./data/basic-app-${type}/locales`)],
-            },
-            options,
-          ),
-        ) as LoaderPlugin
-      }
 
       beforeEach(() => {
         // mock vite-plugin `this` scope
@@ -38,14 +28,14 @@ describe('basic', () => {
       }
 
       it('should generate the structure', async () => {
-        const load = newPlugin().load
+        const load = factory({ paths: [appLocalesDir] }).load
         const res = (load as any).call(thisScope, resolvedVirtualModuleId) as string
         const resStore = await import(esm(res))
         assertCommon(resStore)
       })
 
       it('should process include', () => {
-        const load = newPlugin({ include: ['**/*.json'] }).load
+        const load = factory({ paths: [appLocalesDir], include: ['**/*.json'] }).load
         thisScope.addWatchFile = function (path) {
           expect(path).not.toMatch(/main\.nonjson/)
         }
@@ -54,7 +44,10 @@ describe('basic', () => {
       })
 
       it('should not process files that are excluded', async () => {
-        const load = newPlugin({ include: [`**/*.${type}`, `!**/exclude.${type}`] }).load
+        const load = factory({
+          paths: [appLocalesDir],
+          include: [`**/*.${type}`, `!**/exclude.${type}`],
+        }).load
         thisScope.addWatchFile = function (path) {
           expect(path).not.toMatch(/exclude\.json/)
         }
--- src/__tests__/pathOverride.test.ts
+++ src/__tests__/pathOverride.test.ts
@@ -1,36 +1,43 @@
-import { beforeEach, describe, it } from 'vitest'
+/* eslint-disable unicorn/prefer-module */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import * as path from 'node:path'
+
+import { beforeEach, describe, expect, it } from 'vitest'
+
+import factory, { resolvedVirtualModuleId } from '..'
+import { esm, ThisScope } from './util'
 
 describe('pathOverride', () => {
   for (const type of ['yaml', 'json']) {
+    const appLocalesDir = path.join(__dirname, `./data/override-app-${type}/locales`)
     describe(type, () => {
-      beforeEach(function (done) {
-        //mock webpack loader this scope
-        const emptFn = () => {}
+      let thisScope: ThisScope
+
+      beforeEach(() => {
+        // mock vite-plugin `this` scope
         thisScope = {
-          addDependency: emptFn,
-          addContextDependency: emptFn,
-          cacheable: emptFn,
-          resource: path.join(__dirname, `./data/override-app-${type}/locales/index.js`),
+          addWatchFile: () => {},
         }
-        done()
       })
 
-      it('should simply be the the app locales', () => {
-        const res = loader.call(thisScope, resolvedVirtualModuleId)
-        const resStore = eval(res)
+      it('should load the app locales', async () => {
+        const load = factory({ paths: [appLocalesDir] }).load
+        const res = (load as any).call(thisScope, resolvedVirtualModuleId) as string
+        const resStore = (await import(esm(res))) as any
         expect(resStore.en.foo.test).toStrictEqual('app foo.test en')
         expect(resStore.en.main.sub.subsub.slugslug).toStrictEqual('app sub.subsub.slugsub en')
         expect(resStore['zh-cn'].foo.test).toStrictEqual('app foo.test zh-cn')
-        assert.strictEqual(
-          resStore['zh-cn'].main.sub.subsub.slugslug,
+        expect(resStore['zh-cn'].main.sub.subsub.slugslug).toStrictEqual(
           'app sub.subsub.slugsub zh-cn',
         )
       })
 
-      it('should merge app over any libraries', () => {
-        thisScope.query = '?{overrides: ["../node_modules/lib/locales"]}'
-        const res = loader.call(thisScope, resolvedVirtualModuleId)
-        const resStore = eval(res)
+      it('should merge appLocales over any libraries', async () => {
+        const load = factory({
+          paths: [path.join(appLocalesDir, '../node_modules/lib/locales'), appLocalesDir],
+        }).load
+        const res = (load as any).call(thisScope, resolvedVirtualModuleId) as string
+        const resStore = await import(esm(res))
         expect(resStore.en.main.sub.test).toStrictEqual('lib sub.test en')
         expect(resStore.en.main.sub.subsub.test).toStrictEqual('lib sub.subsub.test en')
         expect(resStore.en.main.sub.slug).toStrictEqual('app sub.slug en')
@@ -39,8 +46,7 @@ describe('pathOverride', () => {
         expect(resStore['zh-cn'].main.sub.test).toStrictEqual('lib sub.test zh-cn')
         expect(resStore['zh-cn'].main.sub.subsub.test).toStrictEqual('lib sub.subsub.test zh-cn')
         expect(resStore['zh-cn'].main.sub.slug).toStrictEqual('app sub.slug zh-cn')
-        assert.strictEqual(
-          resStore['zh-cn'].main.sub.subsub.slugslug,
+        expect(resStore['zh-cn'].main.sub.subsub.slugslug).toStrictEqual(
           'app sub.subsub.slugsub zh-cn',
         )
       })
